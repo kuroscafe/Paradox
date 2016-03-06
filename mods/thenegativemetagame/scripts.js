7cf810056290f4a496507d83d5594d327fe2c974
XpRienzo <anubhavkumarc@gmail.com>
2016-03-06 11:28:37 +0530

    improve TNM script
    shortened the code.

--- mods/thenegativemetagame/scripts.js
+++ mods/thenegativemetagame/scripts.js
@@ -3,30 +3,13 @@ exports.BattleScripts = {
     {
         for (var i in this.data.Pokedex) 
         {
-            if(this.modData('Pokedex', i).baseStats['hp']>145)
-                this.modData('Pokedex', i).baseStats['hp']=5;
-            else
-                this.modData('Pokedex', i).baseStats['hp']=150-this.data.Pokedex[i].baseStats['hp'];
-            if(this.modData('Pokedex', i).baseStats['atk']>145)
-                this.modData('Pokedex', i).baseStats['atk']=5;
-            else
-                this.modData('Pokedex', i).baseStats['atk']=150-this.data.Pokedex[i].baseStats['atk'];
-            if(this.modData('Pokedex', i).baseStats['def']>145)
-                this.modData('Pokedex', i).baseStats['def']=5;
-            else
-                this.modData('Pokedex', i).baseStats['def']=150-this.data.Pokedex[i].baseStats['def'];
-            if(this.modData('Pokedex', i).baseStats['spa']>145)
-                this.modData('Pokedex', i).baseStats['spa']=5;
-            else
-                this.modData('Pokedex', i).baseStats['spa']=150-this.data.Pokedex[i].baseStats['spa'];
-            if(this.modData('Pokedex', i).baseStats['spd']>145)
-                this.modData('Pokedex', i).baseStats['spd']=5;
-            else
-                this.modData('Pokedex', i).baseStats['spd']=150-this.data.Pokedex[i].baseStats['spd'];
-            if(this.modData('Pokedex', i).baseStats['spe']>145)
-                this.modData('Pokedex', i).baseStats['spe']=5;
-            else
-                this.modData('Pokedex', i).baseStats['spe']=150-this.data.Pokedex[i].baseStats['spe'];
+            for(var j in this.data.Pokedex[i].baseStats)
+            {
+                if(this.modData('Pokedex', i).baseStats['j]>145)
+                    this.modData('Pokedex', i).baseStats['j]=5;
+                else
+                    this.modData('Pokedex', i).baseStats[j]=150-this.data.Pokedex[i].baseStats[j];
+            }
         }
     }
 };
