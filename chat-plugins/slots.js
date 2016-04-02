9bbc36669bf10255e7d05231d401117426b09167
BlazingAura <tallha.mulla@gmail.com>
2016-04-02 16:44:15 +0500

    Create slots.js

---
+++ chat-plugins/slots.js
@@ -0,0 +1,103 @@
+// I know the codes a bit off, but meh.
+
+/**
+ * 
+ * Slots.js Made By Dragotic. 
+ * Slots is a casino game.
+ *
+ **/
+
+'use strict';
+
+// To get hash colors of the names
+const color = require('../config/color');
+
+// Available slots for the game
+const slots = {
+    'bulbasaur': 3,
+    'squirtle': 6,
+    'charmander': 9,
+    'pikachu': 12,
+    'eevee': 15,
+    'snorlax': 18,
+    'dragonite': 21,
+    'mew': 24,
+    'mewtwo': 27,
+};
+
+// Trozei sprites for each pokemon
+const slotsTrozei = {
+    'bulbasaur': 'http://www.pokestadium.com/assets/img/sprites/misc/trozei/bulbasaur.gif',
+    'squirtle': 'http://www.pokestadium.com/assets/img/sprites/misc/trozei/squirtle.gif',
+    'charmander': 'http://www.pokestadium.com/assets/img/sprites/misc/trozei/charmander.gif',
+    'pikachu': 'http://www.pokestadium.com/assets/img/sprites/misc/trozei/pikachu.gif',
+    'eevee': 'http://www.pokestadium.com/assets/img/sprites/misc/trozei/eevee.gif',
+    'snorlax': 'http://www.pokestadium.com/assets/img/sprites/misc/trozei/snorlax.gif',
+    'dragonite': 'http://www.pokestadium.com/assets/img/sprites/misc/trozei/dragonite.gif',
+    'mew': 'http://www.pokestadium.com/assets/img/sprites/misc/trozei/mew.gif',
+    'mewtwo': 'http://www.pokestadium.com/assets/img/sprites/misc/trozei/mewtwo.gif',   
+};
+
+const availableSlots = Object.keys(slots);
+
+function spin() {
+    return availableSlots[Math.floor(Math.random() * availableSlots.length)];
+};
+
+function rng() {
+    return Math.floor(Math.random() * 100);
+};
+
+function display(result, user, slotOne, slotTwo, slotThree) {
+    let display = '<div style="padding: 3px; background: #000000; padding: 5px; border-radius: 5px; text-align: center;">' + 
+    '<center><img src="http://i.imgur.com/p2nObtE.gif" width="300" height="70"></center><br />' +
+    '<center><img style="padding: 3px; border: 1px inset gold; border-radius: 5px; box-shadow: inset 1px 1px 5px white;" src="' + slotsTrozei[slotOne] + '">&nbsp;&nbsp;&nbsp;' + '<img style="padding: 3px; border: 1px inset gold; border-radius: 5px; box-shadow: inset 1px 1px 5px white;" src="' + slotsTrozei[slotTwo] + '">&nbsp;&nbsp;&nbsp;' + '<img style="padding: 3px; border: 1px inset gold; border-radius: 5px; box-shadow: inset 1px 1px 5px white;" src="' + slotsTrozei[slotThree] + '"></center>' +
+    '<font style="color: white;"><br />';
+    if (!result) {
+        display += 'Aww... bad luck, <b><font color="' + color(user) + '">' + user + '</font></b>. Better luck next time!</font>';
+    }
+    if (result) {
+        display += 'Congratulations, <b><font color="' + color(user) + '">' + user + '</font></b>. You have won ' +
+        slots[slotOne] + ' bucks!!</font>';
+    }
+    return display + '</div>';
+};
+
+exports.commands = {
+    slots: {
+        start: 'spin',
+        spin: function(target, room ,user) {
+            if (room.id !== 'casino') return this.errorReply('Casino games can only be played in the "Casino".');
+            if (!this.canTalk()) return this.errorReply('/slots spin - Access Denied.');
+            
+            const amount = Db('money').get(user.userid, 0);
+            if (amount < 3) return this.errorReply('You don\'t have enough bucks to play this game. You need ' + (3 - amount) + currencyName(amount) + ' more.');
+            
+            const result = spin();
+            const chancePercentage = rng();
+            const chancesGenerated = 70 + availableSlots.indexOf(result) * 3;
+
+            if (chancePercentage >= chancesGenerated) {
+                Db('money').set(user.userid, (amount + slots[result]));
+                return this.sendReplyBox(display(true, user.name, result, result, result));
+            }
+            Db('money').set(user.userid, (amount - 3));
+            return this.sendReplyBox(display(false, user.name, spin(), spin(), spin()));
+        },
+        '': function(target, room, user) {
+            return this.parse('/help slots');
+        },
+    },
+    slotshelp: ['Slots is a casino game. ' + 
+    'It awards the user with varying amount of bucks depending on the streak of pokemon they user gets.' + 
+    'Following Are Slots Winnings: \n' +
+    'Bulbasaur :  5 bucks' + '\n' +
+    'Squirtle  : 10 bucks' + '\n' +
+    'Charmander: 15 bucks' + '\n' +
+    'Pikachu   : 20 bucks' + '\n' +
+    'Eevee     : 25 bucks' + '\n' +
+    'Dragonite : 30 bucks' + '\n' +
+    'Mew       : 35 bucks' + '\n' +
+    'Mewtwo    : 40 bucks' + '\n' +
+    'Use "/slots spin" to play the game.'],
+};
